8eaababc8c4a00cd1b1d5c4225b897aaec8865e1
ShantnuS <shantnu@outlook.com>
2018-06-16 13:17:57 +0100

    descriptions of parameters

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
-username = 'XXXXX'
-password = 'XXXXX'
-client_id = 'XXXXX'
-client_secret = 'XXXXX'
-user_agent = "XXXXX"
-threshold = -2
+##REDDIT BASICS###
+#More information here: https://praw.readthedocs.io/en/latest/getting_started/quick_start.html 
+username = 'XXXXXXXXX' #This is your reddit username, i.e. spez
+password = 'XXXXXXXXX' #This is your reddit password, i.e hunter2
+client_id = 'XXXXXXXXX' #This is the client ID received after creating an application.
+client_secret = 'XXXXXXXXX' #Client secret received after creating an application
+user_agent = "XXXXXXXXX"  #A descriptive user agent to help Reddit determine source of requests 
+
+##KarmaGuardian Parameters###
+threshold = -2 #Minimum comment score before it is deleted 
